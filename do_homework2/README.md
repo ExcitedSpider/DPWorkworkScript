74ef020622ceb76929fbdf018233d8dde9211299
ExcitedQe <213161557@seu.edu.cn>
2018-12-04 19:19:45 +0800

    homework2_person

---
+++ do_homework2/README.md
@@ -0,0 +1,9 @@
+# do_homework2
+
+第二次作业，重复标注数据
+
+脚本用法是：
+
+```powershell
+homework2_person -sen_id 1 -filepath test.csv -outfile out.json
+```
---
+++ do_homework2/homework2_person.ps1
@@ -0,0 +1,23 @@
+
+param(
+    [Parameter(Mandatory="true")]
+    [int]$sen_id,
+    [Parameter(Mandatory="true")]
+    [string]$filepath,
+    [Parameter(Mandatory="true")]
+    [string]$outfile
+)
+
+
+$json=@{}
+$json.sentences=@()
+
+Get-Content $filepath | ConvertFrom-Csv | ForEach-Object {
+    $item=@{}
+    $item.caption=$_.caption
+    $item.video_id="video_{0}" -f ($_.video_id)
+    $item.sen_id=$sen_id
+    $json.sentences+=$item
+}
+
+$json | ConvertTo-Json | Out-File $outfile
